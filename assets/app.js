6f03ec61aa5cc844af7b00fad47525b4d8ec0dd3
CoralieGa33 <coralie.ga33@gmail.com>
2022-02-28 14:55:25 +0100

    install React

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,17 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
 // start the Stimulus application
-import './bootstrap';
+//import './bootstrap';
+
+console.log("App running !!!");
+
+const App = () => {
+    return <h2>Hello React : )</h2>
+};
+
+const rootElement = document.querySelector("#app");
+ReactDOM.render(<App />, rootElement);
