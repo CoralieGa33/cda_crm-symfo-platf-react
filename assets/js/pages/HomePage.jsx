7b9184266527c8ab4c5b72c96b9a20893491c4e4
CoralieGa33 <coralie.ga33@gmail.com>
2022-03-22 19:21:06 +0100

    realistic homepage

--- assets/js/pages/HomePage.jsx
+++ assets/js/pages/HomePage.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 const HomePage = (props) => {
     return ( 
-        <div className="p-5 mb-4 bg-light rounded-3">
-            <div className="container-fluid py-5">
-                <h1 className="display-5 fw-bold">Home page</h1>
-                <p className="col-md-8 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur nihil ut hic voluptas assumenda, vitae iusto temporibus culpa id nemo quisquam sapiente minus aspernatur animi asperiores nostrum expedita rerum iste.</p>
-                <button className="btn btn-primary btn" type="button">Plus</button>
+        <div className="col-md-8 mx-auto p-5 mb-4 bg-light rounded-3">
+            <div className="py-5">
+                <h1 className="text-center display-5 mb-5 fw-bold">React'i'on</h1>
+                <p className="">Simple application de gestion de factures / clients.</p>
+                <p className="">Le backend (invisible pour l'utilisateur, mais à la base du bon fonctionnement de l'application) a été réalisé sous Symfony v6.0 avec l'appui d'API Platform v2.6.</p>
+                <p className="">Le frontend (l'interface utilisateur) a été créé avec React v17.0.2.</p>
             </div>
         </div>
     );
